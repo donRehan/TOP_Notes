375bab1b25e47a61c95f6da99b5f92fd10f9a1d4
DonRehan <30264386+donRehan@users.noreply.github.com>
2023-11-28 23:39:42 +0200

    Finish events handling introduction

---
+++ js_domManipulation/assignment_sections12_7/events_handling/README.md
@@ -0,0 +1,12 @@
+# Event Handling Examples
+
+Folders structure
+.
+├── handling_events/
+├── index.html
+├── main.js
+├── README.md
+└── understanding/
+
+> Each folder contains a practice file for a particular lesson from
+> https://www.javascripttutorial.net/
---
+++ js_domManipulation/assignment_sections12_7/events_handling/main.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JS Event Demo</title>
+</head>
+<body>
+    <div id="container">
+        <button id='btn'>Click Me!</button>
+		<a href="https://www.javascripttutorial.net/">JS Tutorial</a>
+    </div>
+		<script src="main.js"></script>
+</body>
